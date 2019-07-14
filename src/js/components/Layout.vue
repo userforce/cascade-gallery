7ef84b6b734b2a6acca8b3c223f281d3f73b646b
vagrant <vagrant@homestead>
2019-07-14 03:21:01 +0000

    Prepare dependencies and vue component

---
+++ src/js/components/Layout.vue
@@ -0,0 +1,26 @@
+<script>
+    export default {
+        props: {
+            greetings: {
+                type: String,
+                default: "Greetings!",
+            }
+        },
+        data() {
+            return {
+                display_greetings: true,
+            };
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <p v-if="display_greetings">
+            {{ greetings }}
+        </p>
+        <p v-else="display_greetings">
+            Something went wrong!
+        </p>
+    </div>
+</template>
